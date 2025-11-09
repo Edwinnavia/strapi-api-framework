a874519c3441d5c520061e88b928d42916aa3799
Edwinnavia <edwinnavia24@gmail.com>
2025-11-09 13:40:58 -0400

    test(article): validate filtering with logical AND operator ()

--- test/article/test_list_articles.py
+++ test/article/test_list_articles.py
@@ -95,3 +95,19 @@ def test_list_articles_filter_or(strapi_api, article_factory, module_article):
 
     validate.data.list_contains_document_id(response, second_article["documentId"])
     validate.data.item_field_equals(response, second_article["documentId"], "slug", second_article["slug"])
+
+
+def test_list_articles_filter_and(strapi_api, module_article):
+    params = {
+        "filters[$and][0][title][$eq]": module_article["title"],
+        "filters[$and][1][slug][$eq]": module_article["slug"],
+    }
+    url = ArticleEndpoint.get_all(params)
+    response = strapi_api.get(url)
+
+    validate.status.ok(response)
+    validate.schema.validate_response(response, "article", "list_response_schema.json")
+    validate.data.list_count_equals(response, 1)
+    validate.data.list_contains_document_id(response, module_article["documentId"])
+    validate.data.item_field_equals(response, module_article["documentId"], "title", module_article["title"])
+    validate.data.item_field_equals(response, module_article["documentId"], "slug", module_article["slug"])
