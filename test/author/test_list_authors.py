6724e6b14d58a9cdacb2b5b719616a24af7fc4b2
Edwinnavia <edwinnavia24@gmail.com>
2025-11-09 19:43:13 -0400

    test(author): validate list_authors with logical OR filter on name or email

--- test/author/test_list_authors.py
+++ test/author/test_list_authors.py
@@ -172,6 +172,7 @@ def test_list_authors_filter_email_contains(strapi_api, module_author):
     validate.data.list_contains_document_id(response, module_author["documentId"])
     validate.data.item_field_equals(response, module_author["documentId"], "email", module_author["email"])
 
+
 # ============================================================
 # TC-AU-11 - Combined filters: name AND email
 # ============================================================
@@ -192,3 +193,25 @@ def test_list_authors_filter_name_and_email(strapi_api, module_author):
     validate.data.list_contains_document_id(response, module_author["documentId"])
     validate.data.item_field_equals(response, module_author["documentId"], "name", module_author["name"])
     validate.data.item_field_equals(response, module_author["documentId"], "email", module_author["email"])
+
+
+# ============================================================
+# TC-AU-12 - Logical OR: name OR email
+# ============================================================
+@pytest.mark.authors
+@pytest.mark.positive
+@pytest.mark.functional
+def test_list_authors_filter_or(strapi_api, module_author, author_factory):
+    second_author = author_factory()
+    params = {
+        "filters[$or][0][name][$eq]": module_author["name"],
+        "filters[$or][1][email][$eq]": second_author["email"],
+    }
+
+    url = AuthorEndpoint.get_all(params)
+    response = strapi_api.get(url)
+
+    validate.status.ok(response)
+    validate.schema.validate_response(response, "author", "list_response_schema.json")
+    validate.data.list_contains_document_id(response, module_author["documentId"])
+    validate.data.list_contains_document_id(response, second_author["documentId"])
